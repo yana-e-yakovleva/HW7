988169288dfd42bca0ce7ee0b72cdf71de14649a
Yana Yakovleva <yana.e.yakovleva@yandex.ru>
2022-12-13 23:14:45 +0300

    Task1

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -2,6 +2,8 @@
 
 
 Console.Write("Введите элементы через пробел  ");
+
 int[] arr = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
 int count = arr.Count(i => i > 0);
+
 Console.WriteLine($"Кол-во элементов больше нуля: {count}");
