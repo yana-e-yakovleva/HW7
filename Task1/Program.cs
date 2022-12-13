0f49dab978cc8226b64eb0302a94ad94bc4b9829
Yana Yakovleva <yana.e.yakovleva@yandex.ru>
2022-12-13 23:14:11 +0300

    first commit

---
+++ Task1/Program.cs
@@ -0,0 +1,7 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+
+Console.Write("Введите элементы через пробел  ");
+int[] arr = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+int count = arr.Count(i => i > 0);
+Console.WriteLine($"Кол-во элементов больше нуля: {count}");
